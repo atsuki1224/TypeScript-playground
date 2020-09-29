3163d2e2b0b12f6eb24638f67810fa098fe71a03
atsuki1224 <akeve1224@icloud.com>
2020-09-29 23:41:41 +0900

    commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as line from "@line/bot-sdk";
+import express from "express";
+
+const config = {
+  channelAccessToken: hogehoge,
+  channelSecret:      hugahuga,
+};
+
+const app = express();
